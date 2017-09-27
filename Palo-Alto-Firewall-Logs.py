c01a7fa78693e2bfdcb0cb9fe19c3028d6c21b43
justin <justin@centos-linux.shared>
2017-09-27 15:23:11 -0400

    Edit

--- Palo-Alto-Firewall-Logs.py
+++ Palo-Alto-Firewall-Logs.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python
-#
 # Setup instructions (tested on python 2.7):
 #
 # pip install virtualenv
-# virtualenv venv
-# source ./venv/bin/activate
+# virtualenv -p /usr/bin/python2.7 GitHub/Palo-Alto-Firewall-Logs
+# cd GitHub/Palo-Alto-Firewall-Logs
+# source ./bin/activate
 # pip install requests
 # pip install xmltodict
 #
 #
 # Example usage:
 #  ./firewall_logs.py -H 172.16.216.20 -U admin -P PASSWORD --query "(addr in 8.8.8.8)"
+#  ./firewall_logs.py -H 172.16.216.20 -U admin -P PASSWORD --query "(url contains google.com)"
 
 import argparse
 import json
