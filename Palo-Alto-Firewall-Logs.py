565862751b7b9a966b9a3bdceebf384a6e8405c2
justin <jcoeder@gmail.com>
2017-11-12 12:46:50 -0500

    Minor Changes

--- Palo-Alto-Firewall-Logs.py
+++ Palo-Alto-Firewall-Logs.py
@@ -9,16 +9,14 @@
 # pip install requests
 # pip install xmltodict
 #
-#
-# Option #2 (nick's way, automatically creates virtualenv and requirements for you):
-# ./Palo-Alto-Firewall-Logs.sh -H 172.16.216.20 -U admin -P PASSWORD --query "(addr in 8.8.8.8)"
-#
-#
 # Example usage:
 #  ./Palo-Alto-Firewall-Logs.py -H 172.16.216.20 -U admin -P PASSWORD --query "(addr in 8.8.8.8)"
 #  ./Palo-Alto-Firewall-Logs.py -H 172.16.216.20 -U admin -P PASSWORD --query "(url contains google.com)"
 #  # read queries from CSV
 #  ./Palo-Alto-Firewall-Logs.py -H 172.16.216.20 -U admin -P PASSWORD --filename ./input_data.csv
+#
+# Option #2 (nick's way, automatically creates virtualenv and requirements for you):
+# ./Palo-Alto-Firewall-Logs.sh -H 172.16.216.20 -U admin -P PASSWORD --query "(addr in 8.8.8.8)"
 
 import argparse
 import csv
